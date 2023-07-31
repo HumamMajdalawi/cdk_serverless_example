dac952885b90dbe7166b74a5557b25e2b1e71e9c
lendis <lendis@lendiss-MBP.fritz.box>
2023-07-31 16:46:58 +0200

    Initial commit

---
+++ test/cdk-sports.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSports from '../lib/cdk-sports-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sports-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSports.CdkSportsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
